c25fdc686d10f11028f767ab19a5a44a8d9cead1
Miroslav <miroslav.ievtushenko@gmail.com>
2022-07-03 18:43:17 +0200

    First commit

---
+++ src/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main (String args[]) {
+        int num;
+        num = 100;
+        System.out.println("Zmienna num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Wartosc zmiennej num * 2 ") ;
+        System.out.println(num);
+
+    }
+    public class While {
+        public static void main (String args[]) {
+    int n = 10;
+    while(n > 0) {
+        System.out.println("krok " + n);
+        n--;
+    }
+}
+}
+}
