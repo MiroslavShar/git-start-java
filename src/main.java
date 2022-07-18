d073e80f7fb72b597c75da88080b6409e48d31f4
MiroslavShar <miroslav.ievtushenko@gmail.com>
2022-07-18 19:38:31 +0200

    Set

--- src/main.java
+++ src/main.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class main {
     public static void main (String[] args){
-      List<String> list = new ArrayList<String>();
-      list.add("One");
-      list.add("Three");
-      list.add("four");
+        HashSet<Integer> myHashSet = new HashSet<>();
 
-      list.add(1, "two");
-      for(String s: list){
-          System.out.println(s);
-      }
-      System.out.println("-----");
-      list.remove(2);
+        myHashSet.add(1);
+        myHashSet.add(2);
+        myHashSet.add(3);
 
-      for(String s: list) {
-          System.out.println(s);
-      }
+        //We add 1 one more time
+        myHashSet.add(1);
+
+        for (int i : myHashSet){
+            System.out.println(i);
+        }
     }
 }
