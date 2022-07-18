0b948efb9d9dfd7c47df33b9b2f9aba6b4292eb4
MiroslavShar <miroslav.ievtushenko@gmail.com>
2022-07-18 22:20:17 +0200

    HashSet

--- src/main.java
+++ src/main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
@@ -9,14 +6,13 @@ public class main {
         Scanner l = new Scanner(System.in);
         HashSet<String> myHashSet = new HashSet<>();
         int num = in.nextInt();
-        String line = l.nextLine();
-        if (num < 100_000 && num > 0) {
-            for (int j = 0; j <= num; j++) {
+        if (num < 100000 && num > 0) {
+            for (int j = 0; j < num; j++) {
+                String line = l.nextLine();
                 myHashSet.add(line);
             }
-            for (int i = 0; i <= num; i++) {
+            for (int i = 0; i < num; i++){
                 System.out.println(myHashSet.size());
-                System.out.println(myHashSet);
             }
         }
     }
