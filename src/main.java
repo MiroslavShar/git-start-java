7282a6721dc22a32c7ea095e77197f8ae4e477ed
MiroslavShar <miroslav.ievtushenko@gmail.com>
2022-07-21 19:53:46 +0200

    Array

--- src/main.java
+++ src/main.java
@@ -1,12 +1,13 @@
+import java.io.IOException;
 import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        /*Scanner in = new Scanner(System.in);
         Scanner l = new Scanner(System.in);
         HashSet<String> myHashSet = new HashSet<>();
         int num = in.nextInt();
-        if (num < 100000 && num > 0) {
+        if (num > 0 && num < 100000) {
             for (int j = 0; j < num; j++) {
                 String line = l.nextLine();
                 myHashSet.add(line);
@@ -14,6 +15,19 @@ public class main {
             for (int i = 0; i < num; i++){
                 System.out.println(myHashSet.size());
             }
+        }*/
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to store: ");
+        n = sc.nextInt();
+
+        int[] array = new int[10];
+        System.out.println("Enter the elements of the array: ");
+        for(int i = 0; i < n; i++){
+            array[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
         }
     }
 }
